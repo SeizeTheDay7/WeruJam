91288acdb4f0e040140cae7f1a225a45b48bf698
SeizeTheDay7 <biomatrix117@naver.com>
2025-12-17 21:01:39 +0900

    무기 바꾸기

--- Assets/Code/Scripts/Player/PlayerAttack.cs
+++ Assets/Code/Scripts/Player/PlayerAttack.cs
@@ -20,9 +20,17 @@ public class PlayerAttack : MonoBehaviour
     Transform currentWeapon;
     bool isAttacking = false;
 
+    [Header("Debug")]
+    [SerializeField] GameObject[] weaponList;
+    InputAction prevWeaponAction;
+    InputAction nextWeaponAction;
+    int weaponIdx = 0;
+
     void Awake()
     {
         attackAction = InputSystem.actions.FindAction("Attack");
+        prevWeaponAction = InputSystem.actions.FindAction("PrevWeapon");
+        nextWeaponAction = InputSystem.actions.FindAction("NextWeapon");
         animator = GetComponent<Animator>();
     }
 
@@ -30,12 +38,47 @@ public class PlayerAttack : MonoBehaviour
     {
         attackAction.Enable();
         attackAction.performed += OnAttack;
+
+        prevWeaponAction.Enable();
+        nextWeaponAction.Enable();
+        prevWeaponAction.performed += PrevWeapon;
+        nextWeaponAction.performed += NextWeapon;
+
     }
 
     void OnDisable()
     {
         attackAction.Disable();
         attackAction.performed -= OnAttack;
+
+        prevWeaponAction.Disable();
+        nextWeaponAction.Disable();
+        prevWeaponAction.performed -= PrevWeapon;
+        nextWeaponAction.performed -= NextWeapon;
+    }
+
+    private void PrevWeapon(InputAction.CallbackContext ctx)
+    {
+        weaponIdx--;
+        weaponPrefab = weaponList[(weaponIdx + weaponList.Length) % weaponList.Length];
+        SetDummyWeapon();
+    }
+
+    private void NextWeapon(InputAction.CallbackContext ctx)
+    {
+        weaponIdx++;
+        weaponPrefab = weaponList[(weaponIdx + weaponList.Length) % weaponList.Length];
+        SetDummyWeapon();
+    }
+
+    private void SetDummyWeapon()
+    {
+        foreach (Transform child in attackProjectilePoint)
+        {
+            Destroy(child.gameObject);
+        }
+
+        Instantiate(weaponPrefab, attackProjectilePoint);
     }
 
     [Button]

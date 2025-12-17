8097716420ea288d0443b7c9aebb9f11df9a4dce
SeizeTheDay7 <biomatrix117@naver.com>
2025-12-17 19:44:05 +0900

    적 스폰 개선
    - 소소한 최적화
    - 지형 예외 처리
    - 스폰할 때 툭 튀어나오지 않고 지하에서 올라옴

--- Assets/Code/Scripts/Enemy/Enemy.cs
+++ Assets/Code/Scripts/Enemy/Enemy.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
 using UnityEngine.AI;
+using DG.Tweening;
 
 public class Enemy : MonoBehaviour
 {
-    NavMeshAgent agent;
+    [HideInInspector] public NavMeshAgent agent;
+    [SerializeField] Transform graphic;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Init()
+    {
+        // underground에서 기어올라와
+        graphic.DOLocalMoveY(0, 1f).SetEase(Ease.Linear);
     }
 
     public void SetTarget(Transform target)
--- Assets/Code/Scripts/Enemy/EnemyManager.cs
+++ Assets/Code/Scripts/Enemy/EnemyManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
+using UnityEngine.AI;
 
 public class EnemyManager : MonoBehaviour
 {
@@ -11,27 +12,37 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] GameObject enemyPrefab;
 
     [Header("Parameter")]
-    [SerializeField] float spawnAmount = 8;
+    [SerializeField] int spawnAmount = 8;
     [SerializeField] float spawnInterval = 3f;
+    [SerializeField] float followTargetInterval = 0.1f;
+    [SerializeField] float navmeshSampleRadius = 5f;
 
     [Header("Calculation")]
+    [SerializeField] float mapMaxHeight = 90f;
+    [SerializeField] float checkRayLength = 100f;
+    int navmeshAreaMask = NavMesh.AllAreas;
+    bool canUpdateTarget = true;
     bool canSpawn = true;
     List<Enemy> enemies = new();
 
     void Update()
     {
-        UpdateTarget();
-        if (!canSpawn) return;
-        StartCoroutine(CoSpawn());
+        if (canUpdateTarget) StartCoroutine(CoUpdateTarget());
+        if (canSpawn) StartCoroutine(CoSpawn());
     }
 
-    private void UpdateTarget()
+    private IEnumerator CoUpdateTarget()
     {
+        canUpdateTarget = false;
         foreach (Enemy enemy in enemies)
         {
             if (enemy == null) continue;
+            if (!enemy.agent.enabled) continue;
             enemy.SetTarget(player.transform);
         }
+        yield return new WaitForSeconds(followTargetInterval);
+        canUpdateTarget = true;
+
     }
 
     private IEnumerator CoSpawn()
@@ -40,14 +51,31 @@ public class EnemyManager : MonoBehaviour
 
         for (int i = 0; i < spawnAmount; i++)
         {
-            float t = i / spawnAmount + Random.Range(-0.05f, 0.05f);
-            Vector3 spawnPosition = splineContainer.EvaluatePosition(t);
+            Vector3 spawnPosition = GetSpawnPoint(i);
+            if (spawnPosition == Vector3.zero) continue;
             Enemy enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity).GetComponent<Enemy>();
-            enemy.SetTarget(player.transform);
+            enemy.Init();
             enemies.Add(enemy);
         }
 
         yield return new WaitForSeconds(spawnInterval);
         canSpawn = true;
+    }
+
+    private Vector3 GetSpawnPoint(int i)
+    {
+        float t = (float)i / spawnAmount + Random.Range(-0.05f, 0.05f);
+        Vector3 point = splineContainer.EvaluatePosition(t);
+        point.y = mapMaxHeight;
+        Ray ray = new Ray(point, Vector3.down);
+        Debug.DrawRay(ray.origin, ray.direction * checkRayLength, Color.green, 1f);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, checkRayLength))
+        {
+            if (NavMesh.SamplePosition(hitInfo.point, out NavMeshHit navmeshHit, navmeshSampleRadius, navmeshAreaMask))
+            {
+                return navmeshHit.position;
+            }
+        }
+        return Vector3.zero;
     }
 }
